77a5c2b5a1966774ed2e82f2a158b1968b5f2b9f
Shriyash <ghimires1@my.smccd.edu>
2026-06-24 00:36:56 -0700

    chore: apply client url .env variable

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8080;
 
 app.use(
   cors({
-    origin: [`http://localhost:5173`], // ensures onlt the request ocming from this address is accepted
+    origin: [process.env.CLIENT_URL], // ensures onlt the request ocming from this address is accepted
   }),
 );
 
